24487f9c8688c9e0850242d9aaf2e83388f8af1e
Jerome Grondin <jerome.grondin.fr@gmail.com>
2017-10-04 12:02:23 +0200

    Add branch on layout

--- src/cwLayoutJumpAndMerge.js
+++ src/cwLayoutJumpAndMerge.js
@@ -14,6 +14,9 @@
     cwLayoutJumpAndMerge.prototype.drawAssociations = function(output, associationTitleText, object) {
         /*jslint unparam:true*/
         var fakeItem, obj, nextLayout;
+        if (cwApi.queryObject.isCreatePage()) {
+            return;
+        }
         obj = cwLayoutJumpAndMerge.getNextLayoutNodeAndItems(this, this.viewSchema, [object]);
 
         if (!cwApi.isUndefined(obj)){
@@ -27,31 +30,49 @@
     };
 
     cwLayoutJumpAndMerge.getNextLayoutNodeAndItems = function(node, schema, sources){
-        var i, j, item, tgt, nextNodeId, associationSchema, nextLayout, targets = [], itemsToDisplay = [], distinct = {};
-
-        if (node.mmNode.SortedChildren.length > 1){
-            return;
-        }
+        var i, j, k, obj, item, tgt, nextNodeId, associationSchema, nextLayout, targets = [],
+            itemsToDisplay = [], distinct = {},
+            poppedObj, nodes;
         
         if (node.mmNode.LayoutName === 'cwLayoutJumpAndMerge'){
             for(i=0; i<sources.length; i+=1){
                 item = sources[i];
                 targets = targets.concat(item.associations[node.mmNode.NodeID]);
             }
-            nextNodeId = node.mmNode.SortedChildren[0].NodeId;  
-            associationSchema = cwApi.ViewSchemaManager.getNode(schema, nextNodeId);
-            nextLayout = new cwApi.cwLayouts[associationSchema.LayoutName](associationSchema.LayoutOptions, schema);
-            return cwLayoutJumpAndMerge.getNextLayoutNodeAndItems(nextLayout, schema, targets);
+
+            for(k=0; k<node.mmNode.SortedChildren.length; k+=1){
+                nextNodeId = node.mmNode.SortedChildren[k].NodeId;  
+                associationSchema = cwApi.ViewSchemaManager.getNode(schema, nextNodeId);
+                nextLayout = new cwApi.cwLayouts[associationSchema.LayoutName](associationSchema.LayoutOptions, schema);
+                obj = cwLayoutJumpAndMerge.getNextLayoutNodeAndItems(nextLayout, schema, targets);
+
+                while(obj.data.length > 0){
+                    poppedObj = obj.data.pop();
+                    if(!distinct.hasOwnProperty(poppedObj.object_id)){
+                        distinct[poppedObj.object_id] = poppedObj;
+                        itemsToDisplay.push(poppedObj);
+                    }
+                }
+            }
+            return {
+                layout: obj.layout,
+                data: itemsToDisplay
+            }
         }
         // set itemsToDisplay
 
         for(i=0; i<sources.length; i+=1){
             item = sources[i];
-            for(j=0; j<item.associations[node.mmNode.NodeID].length; j+=1){
-                tgt = item.associations[node.mmNode.NodeID][j];
-                if (!distinct.hasOwnProperty(tgt.object_id)){
-                    distinct[tgt.object_id] = tgt;
-                    itemsToDisplay.push(tgt);
+            if (item !== undefined){
+                nodes = item.associations[node.mmNode.NodeID];
+                if (nodes !== undefined){
+                    for(j=0; j<nodes.length; j+=1){
+                        tgt = nodes[j];
+                        if (!distinct.hasOwnProperty(tgt.object_id)){
+                            distinct[tgt.object_id] = tgt;
+                            itemsToDisplay.push(tgt);
+                        }
+                    }
                 }
             }
         }
